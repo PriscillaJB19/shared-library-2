7fbfc9fed44bd8734ee08674e9473d955cfe83e5
PriscillaJB19 <110067340+PriscillaJB19@users.noreply.github.com>
2022-09-20 22:16:39 -0600

    Update dockerLib.groovy

--- vars/dockerLib.groovy
+++ vars/dockerLib.groovy
@@ -1,14 +1,15 @@
 def build(Map params){
-    powershell "docker build -f ${params.DockerfilePath} -t ${params.DockerImage} ${params.DockerArgs} ${params.DockerContext}"
+    sh echo "Hola"
+    sh "docker build -f ${params.DockerfilePath} -t ${params.DockerImage} ${params.DockerArgs} ${params.DockerContext}"
 }
 
 def push(Map params){
-    powershell "docker push ${params.DockerImage}"
+    sh "docker push ${params.DockerImage}"
 }
 
 def promoter(Map params){
-    powershell "docker pull ${params.DockerImage}"
-    powershell "docker tag ${params.DockerImage} ${params.DockerNewImage}"
-    powershell "docker push ${params.DockerNewImage}"
+    sh "docker pull ${params.DockerImage}"
+    sh "docker tag ${params.DockerImage} ${params.DockerNewImage}"
+    sh "docker push ${params.DockerNewImage}"
 }
 
