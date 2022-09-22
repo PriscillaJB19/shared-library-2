f510ee6c82bdb4695b116809aafda1fe8457efda
PriscillaJB19 <ana.jimenez@grupobabel.com>
2022-09-21 21:53:09 -0600

    cambios

--- vars/dockerLib.groovy
+++ vars/dockerLib.groovy
@@ -1,14 +1,20 @@
 def build(Map params){
+    dockerfile {
     sh "docker build -f ${params.DockerfilePath} -t ${params.DockerImage} ${params.DockerArgs} ${params.DockerContext}"
+    }
 }
 
 def push(Map params){
+    dockerfile {
     sh "docker push ${params.DockerImage}"
+    }
 }
 
 def promoter(Map params){
+    dockerfile {
     sh "docker pull ${params.DockerImage}"
     sh "docker tag ${params.DockerImage} ${params.DockerNewImage}"
     sh "docker push ${params.DockerNewImage}"
+    }
 }
 
